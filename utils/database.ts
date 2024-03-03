d91424414b380880a697b45049d713e061f46c7f
Woohyeok Baek <qordngur156@gmail.com>
2024-03-03 19:17:32 +0900

    Deploy: 워크플로우 환경변수 셋팅

--- utils/database.ts
+++ utils/database.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb"
 // MongoDB랑 연결하는 유틸코드
 // 웹패이지 접속시 connet()가 매번 실행되지않고 접속시 처음 1회만 실행되게함
 // MongoClient.connect()의 결과를 변수 connectDB에 할당후, export함(다른곳에서 사용가능하게)
-const url = process.env.NEXT_PUBLIC_MONGODB_URL
+const url = process.env.MONGODB_URL
 
 if (!url) {
     throw new Error("MONGODB_URL 환경변수 에러");
