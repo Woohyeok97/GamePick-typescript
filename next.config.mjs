0917564e95a18ca7635bc1bff9fbdeeb4da079d3
Woohyeok Baek <qordngur156@gmail.com>
2024-03-15 17:01:14 +0900

    Chore: env 개발/배포 환경별 셋팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode : false,
-    images : {
-        domains: ['s3.ap-northeast-2.amazonaws.com', 'lh3.googleusercontent.com', 'ssl.pstatic.net', 'k.kakaocdn.net'],
-    }
+  reactStrictMode : false,
+  images: {
+    domains: [
+      's3.ap-northeast-2.amazonaws.com',
+      'lh3.googleusercontent.com',
+      'ssl.pstatic.net',
+      'k.kakaocdn.net',
+      't1.kakaocdn.net',
+    ],
+  },
 };
 
 export default nextConfig;
